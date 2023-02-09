0e4ef5427a16abc2ee8bcf21959945cc0b9890e1
Hunter Andersen <andersen.hunter42@gmail.com>
2023-02-09 13:02:48 -0800

    Working file server using express

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const heroData = {
     ]
 }
 
+let nextId = heroData.heros.length;
+
 fs.writeFile("heros.json", JSON.stringify(heroData), (err) => {
     if (err) console.error(err);
     console.log("Wrote data to the hero file");
@@ -43,6 +45,39 @@ server.get("/heros", (req, res) => {
         res.json(JSON.parse(data));
         res.end();
     });
+});
+
+server.post("/heros", (req, res) => {
+    console.log("Attempting to handle the heros POST endpoint");
+
+    //Read the file contents
+    fs.readFile("heros.json", (err, data) => {
+        if (err) throw err;
+
+        //Write that new content into the file
+        const parsedData = JSON.parse(data);
+        //Generate the new id for the new hero
+        //const newId = parsedData.heros[parsedData.heros.length - 1].id + 1;
+        const newHero = {
+            id: nextId++,
+            ...req.body
+        }
+        parsedData.heros.push(newHero);
+        console.log("new data", parsedData);
+
+        //Insert the new data into the file's data
+        fs.writeFile("heros.json", JSON.stringify(parsedData), (err) => {
+            if (err) throw err;
+
+            console.log(`Wrote ${newHero.name} into the file`);
+            res.status(200);
+            res.end(`Updated heros file to include ${newHero.name}`);
+        });
+        
+    });
+    //Send back whether or not the file was written successfully
+    
+
 });
 
 server.post("/echo", (req, res) => {
