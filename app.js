97d917e317ffaecfc132b69160f6d4aee3e081ee
Hunter Andersen <andersen.hunter42@gmail.com>
2023-02-09 12:07:31 -0800

    Set up heros file and get endoint

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
+const fs = require("fs");
 const express = require("express");
 
+//Add some data to a file
+const heroData = {
+    heros: [
+        {
+            id: 0,
+            name: "Ironman",
+            catchphrase: "I am Ironman",
+            isGood: true
+        },
+        {
+            id: 1,
+            name: "Gandalf",
+            catchphrase: "You shall not pass",
+            isGood: true
+        }
+    ]
+}
+
+fs.writeFile("heros.json", JSON.stringify(heroData), (err) => {
+    if (err) console.error(err);
+    console.log("Wrote data to the hero file");
+});
+
 const server = express();
+server.use(express.json());
 
 server.get("/", (req, res) => {
+    console.log("Attempting to handle the home GET endpoint");
     res.write("<h1>Home: Hunter's Pad</h1>");
     res.status(200);
+    res.end();
+});
+
+server.get("/heros", (req, res) => {
+    console.log("Attempting to handle the heros GET endpoint");
+
+    fs.readFile("heros.json", (err, data) => {
+        if (err) console.error(err);
+
+        res.json(JSON.parse(data));
+        res.end();
+    });
+});
+
+server.post("/echo", (req, res) => {
+    console.log("Attempting to handle post");
+    const responseBody =
+     {body: req.body, url: req.url, method: req.method};
+
+    res.status(200);
+    res.json(responseBody);
+    //res.write(JSON.stringify(responseBody));
     res.end();
 });
 
